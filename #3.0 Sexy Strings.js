42e86b5572efebc08c7dc834a6dd421281985118
leeedev <leeesangheee@gmail.com>
2020-12-19 01:01:21 +0900

    Create #3.0 Sexy Strings.js

---
+++ #3.0 Sexy Strings.js
@@ -0,0 +1,10 @@
+// `${name}`
+const sayHi = (name) => `Hello, ${name}`;
+
+console.log(sayHi("sanghee")); // Hello, sanghee
+
+// `${function}`
+console.log(`1 + 2 = ${1 + 2}`); // 1 + 2 = 3
+
+const add = (a, b) => a + b;
+console.log(`1 + 2 = ${add(1, 2)}`); // 1 + 2 = 3
