a27edd4cc365e359255c14d5f1d995f2a9a915f2
leeedev <leeesangheee@gmail.com>
2020-12-22 18:36:09 +0900

    Create #13.4 trim, trimStart, trimEnd.js

---
+++ #13.4 trim, trimStart, trimEnd.js
@@ -0,0 +1,7 @@
+const string = "   hello   ";
+
+console.log(string.trimStart()); // "hello   "
+console.log(string.trimEnd()); // "   hello"
+console.log(string.trim()); // "hello"
+
+console.log(string); // "   hello   "
